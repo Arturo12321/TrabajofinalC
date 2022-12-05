e27101a901f9150d3c53af881c0f61afb87fff34
ALEXANDRO <ALEXANDRO@192.168.1.8>
2022-12-05 00:25:40 -0500

    Compañia-alex

--- src/main/java/com/registro/usuarios/controlador/CompañiaControlador.java
+++ src/main/java/com/registro/usuarios/controlador/CompañiaControlador.java
@@ -1,5 +1,64 @@
 package com.registro.usuarios.controlador;
 
-public class CompañiaControlador {
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.registro.usuarios.modelo.Compañia;
+import com.registro.usuarios.servicio.*;
+
+@Controller
+@RequestMapping("/compañias")
+@SessionAttributes("compañia")
+public class CompañiaControlador {
+	@Autowired
+	@Qualifier("compañia")
+	CompañiaService compañiaService;
+	@RequestMapping("/listar")
+	public String listar (Model model ) {
+		List<Compañia> compañias =compañiaService.listar();
+		model.addAttribute("compañia",compañias);
+		model.addAttribute("titulo","Lista de Compañias");
+		return "compañiaListar";
+	}
+	@RequestMapping("/form")
+	public String formulario (Map<String, Object> model) {
+		Compañia compañia = new Compañia();
+		model.put("compañia",compañia);
+		model.put("btn", "Crear Compañia");
+		return "compañiaForm";
+	}
+	@RequestMapping("/form/{id}")
+	public String actualizar (@PathVariable("id") Long id,Model model) {
+		model.addAttribute("compañia",compañiaService.buscar(id));
+		model.addAttribute("btn","Actualiza Registro");
+		return "compañiaForm";
+	}
+	@RequestMapping(value="/insertar" ,method=RequestMethod.POST )
+	public String guardar(@Validated Compañia compañia,BindingResult result,Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("ERROR","Error al enviar registro");
+			compañia = new Compañia();
+			model.addAttribute("compañia",compañia);
+			model.addAttribute("btn", "Crear Compañia");
+			return "compañiaForm";
+		}else {
+		compañiaService.guardar(compañia);
+		return "redirect:/compañias/listar";
+		}
+	}
+	@RequestMapping("/eliminar/{id}")
+	public String eliminar(@PathVariable("id") Long id) {
+		compañiaService.eliminar(id);
+		return"redirect:/compañias/listar";
+	}	
 }
