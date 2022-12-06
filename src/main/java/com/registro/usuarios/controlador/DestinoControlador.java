032da67b4865b47cef6cb2ec33de0474d086e280
Max <max.martinez@tecsup.edu.pe>
2022-12-06 00:19:26 -0500

    sam

--- src/main/java/com/registro/usuarios/controlador/DestinoControlador.java
+++ src/main/java/com/registro/usuarios/controlador/DestinoControlador.java
@@ -1,6 +1,7 @@
 package com.registro.usuarios.controlador;
 
 import java.util.List;
+
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
